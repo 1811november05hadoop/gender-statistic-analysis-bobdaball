acd7b2d9f6b11ac6498a2ea77f8e49b698cdc942
Alexander <amire51117@gmail.com>
2018-12-12 22:16:43 -0600

    test setup question 1

--- Project1/src/test/java/com/revature/test/FEducation30LessTest.java
+++ Project1/src/test/java/com/revature/test/FEducation30LessTest.java
@@ -1,5 +1,8 @@
 package com.revature.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -22,7 +25,7 @@ public class FEducation30LessTest {
 	private MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
 
 	String criterion = "Gross graduation ratio, tertiary, female (%)";
-	
+	String exampleInput = "\"Chile\",\"CHL\",\"Gross graduation ratio, tertiary, female (%)\",\"SE.TER.CMPL.FE.ZS\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"17.36532\",\"21.87258\",\"12.64567\",\"\",\"\",\"16.68547\",\"\",\"\",\"17.39141\",\"18.2503\",\"18.80552\",\"24.19107\",\"22.18887\",\"\",\"22.66161\",\"10.89959\",\"12.6661\",\"\",\"\",";
 	@Before
 	public void setUp() {
 		FEducation30LessMapper mapper = new FEducation30LessMapper();
@@ -45,7 +48,8 @@ public class FEducation30LessTest {
 		 */
 		String exInput = "";
 		mapDriver.withInput(new LongWritable(1), new Text(exInput));
-		mapDriver.withOutput(key, val);
+		mapDriver.withOutput(new Text("Chile"), new IntWritable(2014));
+		mapDriver.runTest();
 	}
 	
 	@Test
@@ -55,12 +59,12 @@ public class FEducation30LessTest {
 	    values.add(new IntWritable(1));
 
 	    /*
-	     * For this test, the reducer's input will be "cat 1 1".
+	     * For this test, the reducer's input will be \"cat 1 1\".
 	     */
 	    reduceDriver.withInput(new Text("cat"), values);
 
 	    /*
-	     * The expected output is "cat 2"
+	     * The expected output is \"cat 2\"
 	     */
 	    reduceDriver.withOutput(new Text("cat"), new IntWritable(2));
 
@@ -74,12 +78,12 @@ public class FEducation30LessTest {
 	  public void testMapReduce() {
 
 	    /*
-	     * For this test, the mapper's input will be "1 cat cat dog" 
+	     * For this test, the mapper's input will be \"1 cat cat dog\" 
 	     */
 	    mapReduceDriver.withInput(new LongWritable(1), new Text("cat cat dog"));
 
 	    /*
-	     * The expected output (from the reducer) is "cat 2", "dog 1". 
+	     * The expected output (from the reducer) is \"cat 2\", \"dog 1\". 
 	     */
 	    mapReduceDriver.addOutput(new Text("cat"), new IntWritable(2));
 	    mapReduceDriver.addOutput(new Text("dog"), new IntWritable(1));
